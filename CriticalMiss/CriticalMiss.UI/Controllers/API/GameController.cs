72da23a58284e6381ebb78daf72b203a630730eb
Aj Leino <anthony.leinojr@gmail.com>
2018-01-02 18:20:50 -0500

    Add skeleton controller for game API.

---
+++ CriticalMiss/CriticalMiss.UI/Controllers/API/GameController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CriticalMiss.UI.API.Controllers
+{
+
+    public class GameController : Controller
+    {
+        [HttpGet("/games", Name ="All_Games")]
+        public IActionResult GetAllGames()
+        {
+            return null;
+        }
+
+        [HttpGet("/games/{name}/")]
+        public IActionResult GetGame(string name)
+        {
+            return null;
+        }
+
+        [HttpPost("/games")]
+        [ValidateAntiForgeryToken]
+        public IActionResult CreateNewGame(IFormCollection game)
+        {
+            return null;
+        }
+
+        [HttpPut("/games/{name}")]
+        [ValidateAntiForgeryToken]
+        public IActionResult EditGame(string name, IFormCollection gameCollection)
+        {
+            return null;
+        }
+
+        [HttpDelete("/games/{name}")]
+        public IActionResult DeleteGame(string name)
+        {
+            return null;
+        }
+    }
+}
