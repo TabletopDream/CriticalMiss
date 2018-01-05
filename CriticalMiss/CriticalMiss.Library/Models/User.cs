2841f03adece35277865336bc9dca2004d8a2d6e
tyler hendrick <hendricktyler5@gmail.com>
2018-01-05 12:10:31 -0500

    added values to user model.

--- CriticalMiss/CriticalMiss.Library/Models/User.cs
+++ CriticalMiss/CriticalMiss.Library/Models/User.cs
@@ -6,5 +6,11 @@ namespace CriticalMiss.Library.Models
 {
     class User
     {
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateCreated { get; set; }
     }
 }
