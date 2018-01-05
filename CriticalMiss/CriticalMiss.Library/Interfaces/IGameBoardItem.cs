e0b4ec792f99aafd2546fb36c986ecbdc9e5bd7d
Aj Leino <anthony.leinojr@gmail.com>
2018-01-05 10:43:23 -0500

    Add interface for game board. Update interface for individual game.

--- CriticalMiss/CriticalMiss.Library/Interfaces/IGameBoardItem.cs
+++ CriticalMiss/CriticalMiss.Library/Interfaces/IGameBoardItem.cs
@@ -6,7 +6,11 @@ namespace CriticalMiss.Library.Interfaces
 {
     public interface IGameBoardItem
     {
+        int BoardItemId { get; set; }
+
         string Name { get; set; }
+
+        IImageAsset ImageAsset { get; set; }
 
         bool IsToken { get; set; }
 
