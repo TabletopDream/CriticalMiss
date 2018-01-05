0423280152aaed8ed198265e62d2cb88ba2dbb1d
Aj Leino <anthony.leinojr@gmail.com>
2018-01-05 10:44:45 -0500

    Create repository interfaces for access to library models through data layer.

---
+++ CriticalMiss/CriticalMiss.Library/Repository/Interfaces/IGameBoardRepository.cs
@@ -0,0 +1,11 @@
+﻿using CriticalMiss.Library.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CriticalMiss.Library.Repository.Interfaces
+{
+    public interface IGameBoardRepository : IRepository<IGameBoard>
+    {
+    }
+}
---
+++ CriticalMiss/CriticalMiss.Library/Repository/Interfaces/ITabletopGameRepository.cs
@@ -0,0 +1,12 @@
+﻿using CriticalMiss.Library.Interfaces;
+using CriticalMiss.Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CriticalMiss.Library.Repository.Interfaces
+{
+    public interface ITabletopGameRepository : IRepository<TabletopGame>
+    {
+    }
+}
