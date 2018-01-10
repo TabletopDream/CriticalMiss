59fb551fc5a2c3a1248fbb09bc00b57118f1acf5
tyler hendrick <hendricktyler5@gmail.com>
2018-01-10 13:40:53 -0500

    created board interface.

---
+++ CriticalMiss/CriticalMiss.Common/Interfaces/IBoard.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CriticalMiss.Common.Interfaces
+{
+    interface IBoard
+    {
+        int Width { get; set; }
+        int Height { get; set; }
+        int BoardId { get; set; }
+        int GameId { get; set; }
+    }
+}
