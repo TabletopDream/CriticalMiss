789024c155718273b14ae53e023062354bbd9f51
tyler hendrick <hendricktyler5@gmail.com>
2018-01-10 14:45:13 -0500

    created game database service interface.

---
+++ CriticalMiss/CriticalMiss.WebService.Library/Services/Interfaces/IGameDatabaseService.cs
@@ -0,0 +1,12 @@
+﻿using CriticalMiss.Common.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CriticalMiss.WebService.Library.Services.Interfaces
+{
+    interface IGameDatabaseService : IDatabaseService<IGame>
+    {
+    }
+}
