2a4904da1892ec367acebeb073ab7e62c3ad8d39
tyler hendrick <hendricktyler5@gmail.com>
2018-01-12 13:57:23 -0500

    added functionality to move items, and to adjust size of items.

---
+++ CriticalMiss/CriticalMiss.Library/Models/BoardItem.cs
@@ -0,0 +1,32 @@
+﻿using CriticalMiss.Common.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CriticalMiss.Library.Models
+{
+    public class BoardItem : IBoardItem
+    {
+        public int Id { get; set; }
+        public int BoardId { get; set; }
+        public string Name { get; set; }
+        public bool IsToken { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public int XPos { get; set; }
+        public int YPos { get; set; }
+        public IImageAsset ImageAsset { get; set; }
+
+        public void MoveItem(int x, int y)
+        {
+            XPos += x;
+            YPos += y;
+        }
+
+        public void AdjustSize(int w, int h)
+        {
+            Width = w;
+            Height = h;
+        }
+    }
+}
