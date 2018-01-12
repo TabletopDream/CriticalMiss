e378b4986c11bc975e251ef774f6bcece92163c3
tyler hendrick <hendricktyler5@gmail.com>
2018-01-12 15:19:56 -0500

    created game model.

---
+++ CriticalMiss/CriticalMiss.Library/Models/Game.cs
@@ -0,0 +1,14 @@
+﻿using CriticalMiss.Common.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CriticalMiss.Library.Models
+{
+    public class Game : IGame
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+        public List<IBoard> Boards { get; set; }
+    }
+}
