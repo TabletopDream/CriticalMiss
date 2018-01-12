78adbb00538a09624c03a79a70c3d2a20c5a46ec
tyler hendrick <hendricktyler5@gmail.com>
2018-01-12 11:39:02 -0500

    created board model.

---
+++ CriticalMiss/CriticalMiss.Library/Models/Board.cs
@@ -0,0 +1,16 @@
+﻿using CriticalMiss.Common.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CriticalMiss.Library.Models
+{
+    public class Board : IBoard
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public int BoardId { get; set; }
+        public int GameId { get; set; }
+        public int Pixel { get; set; }
+    }
+}
