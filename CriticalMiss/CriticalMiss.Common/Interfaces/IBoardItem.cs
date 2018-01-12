5af209e20a6ded4d3a963e88ead26ab4b8a63189
tyler hendrick <hendricktyler5@gmail.com>
2018-01-12 11:30:48 -0500

    created board item interface.

---
+++ CriticalMiss/CriticalMiss.Common/Interfaces/IBoardItem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CriticalMiss.Common.Interfaces
+{
+    public interface IBoardItem
+    {
+        int BoardItemId { get; set; }
+
+        int GameBoardId { get; set; }
+
+        string Name { get; set; }
+
+        bool IsToken { get; set; }
+
+        int Width { get; set; }
+
+        int Height { get; set; }
+
+        int XPos { get; set; }
+
+        int YPos { get; set; }
+
+        IImageAsset ImageAsset { get; set; }
+    }
+}
