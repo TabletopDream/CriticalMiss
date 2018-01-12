184ff25914191a877db5ac79eb31c7f356cf0c24
tyler hendrick <hendricktyler5@gmail.com>
2018-01-12 11:16:03 -0500

    added game interface properties.

--- CriticalMiss/CriticalMiss.Common/Interfaces/IGame.cs
+++ CriticalMiss/CriticalMiss.Common/Interfaces/IGame.cs
@@ -6,5 +6,8 @@ namespace CriticalMiss.Common.Interfaces
 {
     public interface IGame
     {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public int Id { get; set; }
     }
 }
