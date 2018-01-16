4736545803d9b4bb1bd13ada9eeb710b0488aa72
hendricktyler5 <hendricktyler5@gmail.com>
2018-01-16 18:12:50 -0500

    added board item controller.

---
+++ CriticalMiss/CriticalMiss.WebService.Library/Controllers/BoardItemController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CriticalMiss.WebService.Library.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/BoardItem")]
+    public class BoardItemController : Controller
+    {
+        // GET: api/BoardItem
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/BoardItem/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+        
+        // POST: api/BoardItem
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+        
+        // PUT: api/BoardItem/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+        
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
