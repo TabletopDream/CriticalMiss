f4bd7ff769981c1b0cad95ea623c3ac6bb53dd6f
hendricktyler5 <hendricktyler5@gmail.com>
2018-01-16 21:38:25 -0500

    implemented get in game controller.

--- CriticalMiss/CriticalMiss.WebService.Library/Controllers/GameController.cs
+++ CriticalMiss/CriticalMiss.WebService.Library/Controllers/GameController.cs
@@ -1,7 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Formatting;
 using System.Threading.Tasks;
+using CriticalMiss.Common.Interfaces;
+using CriticalMiss.Library.Models;
+using CriticalMiss.WebService.Library.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -19,16 +24,27 @@ namespace CriticalMiss.WebService.Library.Controllers
         }
 
         // GET: api/Game/5
-        [HttpGet("{id}", Name = "Get")]
-        public string Get(int id)
+        [HttpGet("{gameName}", Name = "Get")]
+        public IGame Get(string name)
         {
-            return "value";
+            Game game = new Game(name);
+
+            HttpBaseInformation client = new HttpBaseInformation();
+
+            var response = client.Client.GetAsync("api/games/{name}");
+            if (response.IsSuccessStatusCode)
+            {
+                game.GameId = response.Content.ReadAsIntAsync().Result;
+            }
+
+            return game;
         }
         
         // POST: api/Game
         [HttpPost]
-        public void Post([FromBody]string value)
+        public void Post([FromBody]string name)
         {
+            Game game = new Game(name, true);
         }
         
         // PUT: api/Game/5
