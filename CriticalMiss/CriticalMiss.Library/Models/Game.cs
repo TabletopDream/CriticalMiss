a19976a13206f9c1455aac5a7303fbcd1d84bda4
tyler hendrick <hendricktyler5@gmail.com>
2018-01-19 14:26:54 -0500

    added password field to game model.

--- CriticalMiss/CriticalMiss.Library/Models/Game.cs
+++ CriticalMiss/CriticalMiss.Library/Models/Game.cs
@@ -9,5 +9,6 @@ namespace CriticalMiss.Library.Models
     {
         public string GameName { get; set; }
         public int GameId { get; set; }
+        public string Password { get; set; }
     }
 }
