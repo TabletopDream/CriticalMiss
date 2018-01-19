5c5370188ce68cc90afa77370393ecdd76c8ae1b
Rinkal <rdgandhi16@gmail.com>
2018-01-18 20:24:12 -0500

    After Initial Migration

--- CriticalMiss/CriticalMiss.Data/GameBoardItemDBO.cs
+++ CriticalMiss/CriticalMiss.Data/GameBoardItemDBO.cs
@@ -40,11 +40,9 @@ namespace CriticalMiss.Data
         public int Width { get; set; }
 
         [Column("PixelHeight")]
-
         public int Height { get; set; }
 
-        public int Id { get; set; }
-
+        [Column("LocalId")]
         public int LocalId { get;set; }
 
         IImageAsset IBoardItem.ImageAsset { get; set; }
--- CriticalMiss/CriticalMiss.Data/Migrations/20180119011333_Migrate for Initial.Designer.cs
+++ CriticalMiss/CriticalMiss.Data/Migrations/20180119011333_Migrate for Initial.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace CriticalMiss.Data.Migrations
 {
     [DbContext(typeof(CriticalMissDbContext))]
-    partial class CriticalMissDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180119011333_Migrate for Initial")]
+    partial class MigrateforInitial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
