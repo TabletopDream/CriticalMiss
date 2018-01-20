0b53023dca0eee330b350bb8c0f2407aa82c8044
hendricktyler5 <hendricktyler5@gmail.com>
2018-01-20 17:41:17 -0500

    created svg component.

--- CriticalMissNg/src/app/svg-component-module/svg-component-module.module.ts
+++ CriticalMissNg/src/app/svg-component-module/svg-component-module.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SvgComponent } from './svg/svg.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [SvgComponent]
 })
 export class SvgComponentModuleModule { }
