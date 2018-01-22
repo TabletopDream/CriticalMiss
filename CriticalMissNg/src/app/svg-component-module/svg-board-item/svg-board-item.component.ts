3739b7504e976f170862b08357ed21817b5ff3d8
Aj Leino <anthony.leinojr@gmail.com>
2018-01-21 21:25:38 -0500

    Add svg component for items to display on board.

---
+++ CriticalMissNg/src/app/svg-component-module/svg-board-item/svg-board-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-svg-board-item',
+  templateUrl: './svg-board-item.component.html',
+  styleUrls: ['./svg-board-item.component.css']
+})
+export class SvgBoardItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- CriticalMissNg/src/app/svg-component-module/svg-component.module.ts
+++ CriticalMissNg/src/app/svg-component-module/svg-component.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SvgBoardItemComponent } from './svg-board-item/svg-board-item.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [
+    SvgBoardItemComponent
+  ]
 })
 export class SvgComponentModule { }
