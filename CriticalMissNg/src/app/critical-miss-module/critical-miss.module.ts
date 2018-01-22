5d311adfd1bfb3a937d5569232c3593f5c44b565
Aj Leino <anthony.leinojr@gmail.com>
2018-01-21 21:23:02 -0500

    Fix minor merge conflict in critical-miss module definition file.

--- CriticalMissNg/src/app/critical-miss-module/critical-miss.module.ts
+++ CriticalMissNg/src/app/critical-miss-module/critical-miss.module.ts
@@ -1,9 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-<<<<<<< HEAD
-=======
-//import { TestComponentComponent } from './test-component/test-component.component';
->>>>>>> 7b363a753b8c0fcbb9bf65b11b296211a5ca1d22
 import { CriticalMissHttpModule } from '../critical-miss-http/critical-miss-http.module';
 
 @NgModule({
@@ -11,10 +7,6 @@ import { CriticalMissHttpModule } from '../critical-miss-http/critical-miss-http
     CommonModule,
     CriticalMissHttpModule
   ],
-<<<<<<< HEAD
   declarations: []
-=======
-  //declarations: [TestComponentComponent]
->>>>>>> 7b363a753b8c0fcbb9bf65b11b296211a5ca1d22
 })
 export class CriticalMissModule { }
